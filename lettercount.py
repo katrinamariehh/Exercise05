fbc970f907c536f155c80b7bef00995a867ee2f7
Student <user@chromebox-001.(none)>
2014-02-17 11:09:37 -0800

    Lettercount.py working with twain.txt file

--- lettercount.py
+++ lettercount.py
@@ -11,13 +11,13 @@ letters = letters.lower()
 alphabet = [0] * 26
 
 for letter in letters:
-    # if the character is not between a and z, pass
-    if not ord(letter) >= ord('a') and ord(letter) <= ord('z'):
-        pass
-    else:
-    # if the character is between a and z, add 1 to that position in that alphabet list
+    # if the character is between a and z, add 1 to that position in the alphbet list
+    if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
         index = ord(letter) - 97
         alphabet[index] += 1
+    else:
+    # if the character is not between a and z, pass
+        pass
 
 for i in range (len(alphabet)):
     print chr(i + 97), 
